1b91aae22ec1fbf319e9fdacaf5a2ddf3c3aa009
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-03-13 15:36:01 +0100

    creates a rest controller

---
+++ mycoolapp/src/main/java/de/juristan/springboot/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package de.juristan.springboot.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that returns "Hello world"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world";
+    }
+}

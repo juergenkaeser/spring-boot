c2db1d7f657ac133598b2c1db0ed5f6231036bb2
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-04-07 09:50:00 +0200

    adds querying objects with jpa

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/de/juristan/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/de/juristan/cruddemo/dao/StudentDAO.java
@@ -2,9 +2,15 @@ package de.juristan.cruddemo.dao;
 
 import de.juristan.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findByLastName(String theLastName);
+
+    List<Student> findAll();
 }

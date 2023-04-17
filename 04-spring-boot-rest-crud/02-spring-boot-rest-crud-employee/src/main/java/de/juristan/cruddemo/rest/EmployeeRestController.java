37f91353329505d36f48dbe97f0415ab242a1e10
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-04-17 18:07:01 +0200

    adds get single employee by id endpoint

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/de/juristan/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/de/juristan/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import de.juristan.cruddemo.entity.Employee;
 import de.juristan.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,18 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeID}
+    @GetMapping("/employees/{employeeID}")
+    public Employee getEmployee(@PathVariable int employeeID) {
+
+        Employee theEmployee = employeeService.findById(employeeID);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeID);
+        }
+
+        return theEmployee;
     }
 }

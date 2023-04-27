8931aca47cb45ff7aab20472e36ca804aa8442a7
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-04-27 18:57:43 +0200

    adjusts spring.data.rest.default-page-size

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/de/juristan/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/de/juristan/cruddemo/dao/EmployeeRepository.java
@@ -3,4 +3,5 @@ package de.juristan.cruddemo.dao;
 import de.juristan.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}

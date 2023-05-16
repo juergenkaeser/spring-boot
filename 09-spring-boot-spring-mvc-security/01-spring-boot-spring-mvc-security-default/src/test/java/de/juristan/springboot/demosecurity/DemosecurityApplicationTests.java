b1fca99a0350224d94000d2256ef64d8c098750f
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-05-16 17:59:14 +0200

    adds spring mvc security project

---
+++ 09-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/de/juristan/springboot/demosecurity/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package de.juristan.springboot.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

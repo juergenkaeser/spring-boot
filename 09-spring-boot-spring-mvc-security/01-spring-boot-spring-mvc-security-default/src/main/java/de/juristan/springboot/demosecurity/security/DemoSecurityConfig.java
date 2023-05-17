f3c4d8a9540931acd668e3a1457a2f461d4ed2a3
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-05-17 10:24:23 +0200

    adds DemoSecurityConfig with basic configuration

---
+++ 09-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/de/juristan/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,35 @@
+package de.juristan.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    };
+}

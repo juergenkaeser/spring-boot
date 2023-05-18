0e5941c35a024481dc9e29227692a6d1bd4491dc
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-05-18 23:06:19 +0200

    adds logout button and functionality

--- 09-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/de/juristan/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 09-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/de/juristan/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package de.juristan.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -47,7 +48,8 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
-                );
+                )
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }

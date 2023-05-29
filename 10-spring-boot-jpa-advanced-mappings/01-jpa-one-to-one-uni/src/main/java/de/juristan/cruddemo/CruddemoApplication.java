52a691aefdacf7cd4909343fb5cdce2024190b91
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2023-05-29 17:13:38 +0200

    adds DAO to main app and adjusts logging

--- 10-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/de/juristan/cruddemo/CruddemoApplication.java
+++ 10-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/de/juristan/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package de.juristan.cruddemo;
 
+import de.juristan.cruddemo.dao.AppDAO;
+import de.juristan.cruddemo.entity.Instructor;
+import de.juristan.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +16,27 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			System.out.println("Hello World!");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Juri", "Stan", "juri@juristan.de");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("juristan.de/youtube", "BJJ");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor (this will also save the details object because of CascadeType.ALL)
+		System.out.println("Saving the instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+		System.out.println("Instructor saved!");
 	}
 
 }
